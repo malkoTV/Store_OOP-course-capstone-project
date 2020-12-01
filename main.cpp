1cad77dd4dd1ba1bd1e11e67d2a605a295a50286
Tamara Malko <73736618+malkoTV@users.noreply.github.com>
2020-12-01 14:39:33 +0200

    read & write to file - unfinished

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <clocale>
+#include <fstream>
 #include "Item.h"
 #include "Listings.h"
 #include "DayBalance.h"
@@ -8,6 +9,11 @@
 using namespace std;
 
 void Table(Item* array, int n);
+template<class T> void WriteTextFile(T* object, int size, string name = "New file", string path = "", string format = ".txt");
+template<class T> void ReadTextFile(T* object, int size, string name = "New file", string path = "", string format = ".txt");
+
+template<class T> void WriteBinaryFile(T* object, int size, string name = "New file", string path = "", string format = ".dat");
+template<class T> void ReadBinaryFile(T* object, int size, string name = "New file", string path = "", string format = ".dat");
 
 int main() {
 
@@ -15,10 +21,18 @@ int main() {
     //shopItems.Table();
 
     DayBalance balance = DayBalance(5);
-    balance.Table();
+    //balance.Table();
 
     WeekBalance weekBalance = WeekBalance();
-    //weekBalance.Table();
+    weekBalance.Table();
+
+    Item item = Item();
+    item.setPrice(90.0);
+    WriteBinaryFile(&item, 1, "Data file");
+    Item item2 = Item();
+    ReadBinaryFile(&item2, 1);
+
+    cout << "Item price " << item2.getPrice() << endl;
 
     /*for(int i = 179; i < 256; i++)
     {
@@ -32,4 +46,68 @@ int main() {
 
     }
     return 0;
+}
+
+template<class T> void WriteTextFile(T* object, int size, string name, string path, string format)
+{
+    ofstream output;
+    output.open(path + name + format, ofstream::app);
+
+    //todo write try catch block and throw an exception
+    if(output.is_open())
+    {
+        for(int i = 0; i < size; i++)
+        {
+            output.write((char*)&object[i], sizeof(T));
+        }
+    }
+    output.close();
+}
+
+template<class T> void ReadTextFile(T* object, int size, string name, string path, string format)
+{
+    ifstream input;
+    input.open(path + name + format);
+
+    //todo write try catch block and throw an exception
+    if(input.is_open())
+    {
+        for(int i = 0; i < size; i++)
+        {
+            input.read((char*)&object[i], sizeof(T));
+        }
+    }
+    input.close();
+}
+
+template<class T> void WriteBinaryFile(T* object, int size, string name, string path, string format)
+{
+    ofstream output;
+    output.open(path + name + format,ios::out | ios::binary);
+
+    //todo write try catch block and throw an exception
+    if(output.is_open())
+    {
+        for(int i = 0; i < size; i++)
+        {
+            output.write((char*)&object[i], sizeof(T));
+        }
+    }
+    output.close();
+}
+
+template<class T> void ReadBinaryFile(T* object, int size, string name, string path, string format)
+{
+    ifstream input;
+    input.open(path + name + format, ios::out | ios::binary | ios::app);
+
+    //todo write try catch block and throw an exception
+    if(input.is_open())
+    {
+        for(int i = 0; i < size; i++)
+        {
+            input.read((char*)&object[i], sizeof(T));
+        }
+    }
+    input.close();
 }
