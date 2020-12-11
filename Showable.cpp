158a248a0fc691560aac8b1a56e4b2c83a1efa5d
Tamara Malko <73736618+malkoTV@users.noreply.github.com>
2020-12-11 17:08:48 +0200

    menu - tables & search

--- Showable.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by 1 on 30-Nov-20.
-//
-
-#include "Showable.h"
--- main.cpp
+++ main.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
-#include <clocale>
 #include <fstream>
-#include "Item.h"
 #include "Date.h"
-#include "Listings.h"
+#include "Item.h"
+#include "Seller.h"
+#include "Order.h"
 #include "DayBalance.h"
 #include "WeekBalance.h"
+#include "Listings.h"
 
 #include "Exceptions/IndexingException.h"
 #include "Exceptions/LargeIdxException.h"
 #include "Exceptions/NegIdxException.h"
 
+#define N 10
+
 using namespace std;
 
-void Table(Item* array, int n);
+void ShowTable(Listings* listings, WeekBalance* weekBalance, DayBalance* dayBalances);
+void Search(Listings* listings, WeekBalance* weekBalance, DayBalance* dayBalances);
+
 template<class T> void WriteTextFile(T* object, int size, string name = "New file", string path = "", string format = ".txt");
 template<class T> void ReadTextFile(T* object, int size, string name = "New file", string path = "", string format = ".txt");
 
@@ -22,7 +27,13 @@ template<class T> void ReadBinaryFile(T* object, int size, string name = "New fi
 
 int main() {
 
-    /*
+    Item item = Item();
+    cout << "Item " << item << endl;
+
+    Date date = Date();
+    cout << "Date " << date << endl;
+
+
     Listings shopItems = Listings(5);
     //shopItems.Table();
     cout << "Item count " << (int)shopItems << endl;
@@ -37,10 +48,7 @@ int main() {
     {
         ex.PrintMessage();
     }
-    Item item = Item();
-    shopItems = shopItems + item;
-    shopItems.AddItem(item);
-    cout << "Item count " << (int)shopItems << endl;
+
 
     DayBalance balance = DayBalance(5);
     //balance.Table();
@@ -69,53 +77,217 @@ int main() {
     {
         ex.PrintMessage();
     }
-    */
 
-    WeekBalance weekBalance = WeekBalance();
-    weekBalance.getDays()[3].setTotal(40.0);
-    weekBalance.getDays()[3].getOrders()[0].setTotal(50.0);
-    weekBalance.getDays()[3].getOrders()[0].getSeller().setLastName("Malko");
-    weekBalance.getDays()[3].getOrders()[0].getItem().setName("Soup");
-    //weekBalance.getDays()[3].Table();
-    //cout << "Week average sum " << weekBalance.AvgOrderSum() << endl;
-    //cout << "Week average count " << weekBalance.AvgOrderCount() << endl;
 
-    int s = 5;
-    DayBalance* dayBalance = new DayBalance(s);
-    weekBalance.Table();
-    weekBalance.Search("Malko", dayBalance, s);
+    Listings listings = Listings();
+    DayBalance* dayBalances = new DayBalance[N];
 
-    //weekBalance.Table();
 
-    dayBalance[0].Table();
-
-    DayBalance day = DayBalance();
-    day.Contains("string");
-    day.Contains(6);
-
-    Date date = Date();
-    string str = "Current date: ";
-    //cout << str + date << endl;
-    /*Item* item = new Item();
-    item->setPrice(90.0);
-    WriteTextFile(item, 1, "File skdhfwKEFB");
-    Item* item2 = new Item();
-    ReadTextFile(item2, 1, "kdjhglkh");
-
-    cout << "Item price " << item2->getPrice() << endl;*/
-
-    /*for(int i = 179; i < 256; i++)
-    {
-        cout << i << " - ";
-        printf("%c\n", i);
-    } */
-
-    bool temp = false;
+    int temp = 1;
+    //cout << "Enter 1 to go to menu\n";
+    //cin >> temp;
     while(temp)
     {
+        cout << "Menu" << endl;
+        cout << "0 - Exit" << endl;
+        cout << "1 - Create new Object" << endl;
+        cout << "2 - Show table for..." << endl;
+        cout << "3 - Read Object from file" << endl;
+        cout << "4 - Write Object to file" << endl;
+        cout << "5 - Search in..." << endl;
+        cout << "6 - Edit Object" << endl;
 
+        char t;
+        char o;
+        cin >> t;
+
+        int idx;
+
+        switch (t) {
+            case '0':
+                return 0;
+            case '1':
+                break;
+            case '2':
+                ShowTable(&listings, &weekBalance, dayBalances);
+                break;
+            case '3':
+                cout << "What do you want to read from file?" << endl;
+                cout << "0 - quit show\n"
+                     << "1 - Listings\n"
+                     << "2 - Week balance\n"
+                     << "3 - Day balance (from array)\n";
+                cin >> o;
+                switch(o)
+                {
+
+                }
+                break;
+            case '4':
+                break;
+            case '5':
+                Search(&listings, &weekBalance, dayBalances);
+                break;
+            case '6':
+                break;
+            default:
+                cout << "Invalid input" << endl;
+        }
+        system("pause");
     }
+
     return 0;
+}
+
+void ShowTable(Listings* listings, WeekBalance* weekBalance, DayBalance* dayBalances)
+{
+    char o;
+    int idx;
+
+    cout << "What do you want to show?\n";
+    cout << "0 - quit show\n"
+         << "1 - all items in the shop\n"
+         << "2 - week balance\n"
+         << "3 - day balance (from array)\n"
+         << "4 - day balance (from week balance)\n";
+    cin >> o;
+    Showable* spointer;
+    switch(o){
+        case '0':
+            break;
+        case '1':
+            spointer = listings;
+            break;
+        case '2':
+            spointer = weekBalance;
+            break;
+        case '3':
+            cout << "Enter index" << endl;
+            cin >> idx;
+            try{
+                if(idx >= N)
+                {
+                    throw LargeIdxException(idx);
+                }
+                else if(idx < 0)
+                {
+                    throw NegIdxException(idx);
+                }
+
+                spointer = &dayBalances[idx];
+
+            } catch (IndexingException &ex) {
+                ex.PrintMessage();
+            }
+            break;
+        case '4':
+            cout << "Enter index" << endl;
+            cin >> idx;
+            try{
+                if(idx >= W)
+                {
+                    throw LargeIdxException(idx);
+                }
+                else if(idx < 0)
+                {
+                    throw NegIdxException(idx);
+                }
+
+                spointer = &weekBalance[idx];
+
+            } catch (IndexingException &ex) {
+                ex.PrintMessage();
+            }
+            break;
+        default:
+            cout << "Unknown command" << endl;
+            break;
+    }
+    spointer->Table();
+}
+
+void Search(Listings* listings, WeekBalance* weekBalance, DayBalance* dayBalances)
+{
+    char o;
+    int num;
+    string str;
+
+    char mode;
+    cout << "What do you want to search?" << endl;
+    cout << "N - number\tS - string" << endl;
+    cin >> mode;
+    if(mode == 'N')
+    {
+        cin >> num;
+    }
+    else if(mode == 'S')
+    {
+        cin >> str;
+    }
+    else
+    {
+        mode = 'N';
+        cin >> num;
+    }
+
+    int size = 3;
+    Item* itemSearch = new Item[size];
+    DayBalance* daySearch = new DayBalance[size];
+    Order* orderSearch = new Order[size];
+
+    cout << "Where do you want to search?\n";
+    cout << "0 - quit show\n"
+         << "1 - all items in the shop\n"
+         << "2 - week balance\n"
+         << "3 - day balance (from week balance)\n";
+    cin >> o;
+    switch(o){
+        case '0':
+            break;
+        case '1':
+            if(mode == 'N')
+            {
+                listings->Search(num, itemSearch, size);
+            }
+            else if(mode == 'S')
+            {
+                listings->Search(str, itemSearch, size);
+            }
+            break;
+        case '2':
+            if(mode == 'N')
+            {
+                weekBalance->Search(num, daySearch, size);
+            }
+            else if(mode == 'S')
+            {
+                weekBalance->Search(str, daySearch, size);
+            }
+            break;
+        case '3':
+            for(int i = 0; i < N; i++)
+            {
+                if(mode == 'N')
+                {
+                    if(dayBalances->Search(num, orderSearch, size))
+                    {
+                        i = N;
+                    }
+                }
+                else if(mode == 'S')
+                {
+                    if(dayBalances->Search(str, orderSearch, size))
+                    {
+                        i = N;
+                    }
+                }
+            }
+            break;
+        default:
+            cout << "Unknown command" << endl;
+            break;
+    }
+    cout << "Found " << size << " objects" << endl;
 }
 
 template<class T> void WriteTextFile(T* object, int size, string name, string path, string format)
